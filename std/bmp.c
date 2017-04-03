f9a1ce8753dccdac2e5ffc7221e4ea90994cea9e
lancelot <lancelot.b@outlook.com>
2017-04-03 21:41:31 +0800

    030417#02: add: support for bmp v4 format

--- std/bmp.c
+++ std/bmp.c
@@ -123,6 +123,26 @@ struct _bmp_header_t {
 	struct _bmp_info_t info;
 };
 
+struct _bmp_info_v4_pad_t {
+	ivm_uint32_t rmask;			// mask identifying bits of red component
+	ivm_uint32_t gmask;			// mask identifying bits of green component
+	ivm_uint32_t bmask;			// mask identifying bits of blue component
+	ivm_uint32_t amask;			// mask identifying bits of alpha component
+	ivm_uint32_t cs_type;		// color space type
+	ivm_uint32_t redx;			// x coordinate of red endpoint
+	ivm_uint32_t redy;			// y coordinate of red endpoint
+	ivm_uint32_t redz;			// z coordinate of red endpoint
+	ivm_uint32_t greenx;		// x coordinate of green endpoint
+	ivm_uint32_t greeny;		// y coordinate of green endpoint
+	ivm_uint32_t greenz;		// z coordinate of green endpoint
+	ivm_uint32_t bluex;			// x coordinate of blue endpoint
+	ivm_uint32_t bluey;			// y coordinate of blue endpoint
+	ivm_uint32_t bluez;			// z coordinate of blue endpoint
+	ivm_uint32_t gamma_red;		// gamma red coordinate scale value
+	ivm_uint32_t gamma_green;	// gamma green coordinate scale value
+	ivm_uint32_t gamma_blue;	// gamma blue coordinate scale value
+};
+
 ivm_bool_t
 ivm_image_bmp_format(ivm_image_t *image,
 					 ivm_stream_t *output)
@@ -191,10 +211,10 @@ ivm_image_bmp_format(ivm_image_t *image,
 	return suc;
 }
 
-ivm_byte_t * /* decoded data */
+ivm_byte_t *		// decoded data
 _bmp_rle8_decode(ivm_byte_t *cdat,
 				 ivm_size_t size,
-				 ivm_size_t *osize /* output size */,
+				 ivm_size_t *osize,		// output size
 				 const ivm_char_t **err)
 {
 	ivm_stream_t *bufs = ivm_buffer_stream_new(IVM_NULL, 0);
@@ -215,7 +235,7 @@ _bmp_rle8_decode(ivm_byte_t *cdat,
 			tmp = cur[1];
 			ivm_byte_t buf[*cur]; // <= 256
 
-			// IVM_TRACE("size: %d\n", *cur);
+			// IVM_TRACE("comp: %d x %d\n", *cur, tmp);
 
 			for (i = 0; i < *cur; i++) {
 				buf[i] = tmp;
@@ -223,7 +243,8 @@ _bmp_rle8_decode(ivm_byte_t *cdat,
 
 			ivm_stream_write(bufs, buf, sizeof(*buf), *cur);
 		} else switch (*(cur + 1)) {
-			case 1: goto RERROR_END;
+			case 1:
+				goto RERROR_END;
 			case 0:
 				// IVM_TRACE("%d\n", IVM_PTR_DIFF(cur, cdat, ivm_byte_t));
 				break;
@@ -264,6 +285,30 @@ RERROR_END:;
 #define _BMP_COMP_RLE8 1
 #define _BMP_COMP_RLE4 2
 
+IVM_INLINE
+ivm_uint32_t
+_get_ofs(ivm_uint32_t mask)
+{
+	ivm_int_t i = 0;
+	if (!mask) return 0;
+	while (!(mask & 1)) mask >>= 1, i++;
+	return i;
+}
+
+IVM_INLINE
+ivm_pixel_t
+_mix_rgba(ivm_uint32_t r,
+		  ivm_uint32_t g,
+		  ivm_uint32_t b,
+		  ivm_uint32_t a)
+{
+	return
+		((r & 0xff) << 24) |
+		((g & 0xff) << 16) |
+		((b & 0xff) << 8) |
+		(a & 0xff);
+}
+
 ivm_image_t *
 ivm_image_bmp_parse(ivm_stream_t *dat,
 					const ivm_char_t **err)
@@ -271,12 +316,17 @@ ivm_image_bmp_parse(ivm_stream_t *dat,
 	const ivm_char_t *tmp_err = IVM_NULL;
 	struct _bmp_header_t header;
 	ivm_size_t i, dsize = -1, psize;
+	struct _bmp_info_v4_pad_t v4info;
+	ivm_bool_t usev4 = IVM_FALSE; // use v4 version of bmp format
 	
 	ivm_pixel_t *pixels = IVM_NULL,
 				*clr_table = IVM_NULL, *cur;
 
 	ivm_image_t *ret = IVM_NULL;
 	ivm_uint16_t htype;
+
+	ivm_uint32_t pr, pg, pb, pa, // tmp value for rgba
+				 rofs, gofs, bofs, aofs; // offset of rgba(in their masks)
 
 	ivm_byte_t *pdat = IVM_NULL, tmp, *tmpbuf;
 
@@ -291,6 +341,15 @@ ivm_image_bmp_parse(ivm_stream_t *dat,
 		goto RERROR;
 	}
 
+	if (header.info.size > sizeof(header.info)) {
+		// ignore bmp v4 header
+		usev4 = IVM_TRUE;
+		if (ivm_stream_read(dat, &v4info, sizeof(v4info), 1) != 1) {
+			tmp_err = "wrong header";
+			goto RERROR;
+		}
+	}
+
 	// IVM_TRACE("%d\n", header.info.clr_used);
 
 	if (header.ofs != sizeof(htype) + sizeof(header)) {
@@ -299,7 +358,30 @@ ivm_image_bmp_parse(ivm_stream_t *dat,
 		clr_table = STD_ALLOC(sizeof(*clr_table) * header.info.clr_used);
 		IVM_MEMCHECK(clr_table);
 
-		ivm_stream_read(dat, clr_table, sizeof(*clr_table), header.info.clr_used);
+		if (ivm_stream_read(dat, clr_table, sizeof(*clr_table), header.info.clr_used)
+			!= header.info.clr_used) {
+			tmp_err = "unexpected ending";
+			goto RERROR;
+		}
+
+		if (usev4) {
+			// re-mix rgba
+			// IVM_TRACE("rmask: %d\n", v4info.rmask);
+
+			rofs = _get_ofs(v4info.rmask);
+			gofs = _get_ofs(v4info.gmask);
+			bofs = _get_ofs(v4info.bmask);
+			aofs = _get_ofs(v4info.amask);
+
+			for (i = 0; i < header.info.clr_used; i++) {
+				pr = (clr_table[i] & v4info.rmask) >> rofs;
+				pg = (clr_table[i] & v4info.gmask) >> gofs;
+				pb = (clr_table[i] & v4info.bmask) >> bofs;
+				pa = (clr_table[i] & v4info.amask) >> aofs;
+				clr_table[i] = _mix_rgba(pr, pg, pb, pa);
+				// IVM_TRACE("color table: %d: %d, %d, %d, %d\n", clr_table[i], pr, pg, pb, pa);
+			}
+		}
 
 		// IVM_TRACE("clr: %d\n", sizeof(htype) + sizeof(header));
 
@@ -318,7 +400,13 @@ ivm_image_bmp_parse(ivm_stream_t *dat,
 		goto RERROR;
 	}
 
-	// IVM_TRACE("dsize: %d\n", dsize);
+	/*
+	IVM_TRACE("bitmap size: %d\n", header.info.compr_size);
+	IVM_TRACE("hsize: %d\n", header.info.size);
+	IVM_TRACE("ofs: %d\n", header.ofs);
+	IVM_TRACE("clr: %d\n", header.info.clr_used);
+	IVM_TRACE("clr_imp: %d\n", header.info.clr_imp);
+	*/
 
 	// check compression
 	switch (header.info.compr) {
@@ -395,6 +483,7 @@ ivm_image_bmp_parse(ivm_stream_t *dat,
 				 i < psize; i++, cur++) {
 				tmp = pdat[i];
 				*cur = clr_table[tmp];
+				// IVM_TRACE("pix: %d\n", tmp);
 			}
 
 			ret = ivm_image_new(header.info.width, header.info.height, pixels);
--- test/img.ink
+++ test/img.ink
@@ -2,7 +2,7 @@ import std
 import image
 import io
 
-img = image.bmp.parse("test_img.bmp")
+img = image.bmp.parse("test_img1.bmp")
 print(img.width())
 print(img.height())
 
@@ -12,31 +12,34 @@ loc pixel = fn pix: {
 	g: (pix >>> 16) & 0xff,
 	r: (pix >>> 24) & 0xff,
 
-	to_s: fn "(#{base.r}, #{base.g}, #{base.b})",
+	to_s: fn "(#{base.r}, #{base.g}, #{base.b}, #{base.a})",
 	raw: fn (base.a & 0xff) | ((base.b & 0xff) << 8) | ((base.g & 0xff) << 16) | ((base.r & 0xff) << 24)
 }
 
-// get: 960380928
-// set: 3334586367
-
-// get: 960380928
-// set: 3334586367
-
-// 960380928
-
-// 960380928, -960380929
-// 960380928, 3334586367
 for loc i in range(img.width()):
 	for loc j in range(img.height()): {
 		// print(pix(img.get(i, j)))
 		pix = pixel(img.get(i, j))
+		// print(pixel(~img.get(i, j)))
 		// print(img.get(i, j) + ", " + ~img.get(i, j))
 		img.set(i, j, ~img.get(i, j))
 	}
 
 img.encode("bmp", "test_img2.bmp")
 
-image.image(100, 100).encode("bmp", "test_img3.bmp")
+img = image.bmp.parse("test_img3.bmp")
+print(img.width())
+print(img.height())
+
+for loc i in range(img.width()):
+	for loc j in range(img.height()): {
+		pix = pixel(img.get(i, j))
+		img.set(i, j, ~img.get(i, j))
+	}
+
+img.encode("bmp", "test_img4.bmp")
+
+image.image(100, 100).encode("bmp", "test_img5.bmp")
 
 // print(image.bmp.parse("t1.bmp").width())
 
@@ -44,3 +47,5 @@ ret
 
 // -> "num: 200"
 // -> "num: 150"
+// -> "num: 128"
+// -> "num: 128"
